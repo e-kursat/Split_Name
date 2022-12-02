cd95053bdd39d601ea5cb2271c7de8757d296733
Kürşat Emre Özkara <57959688+e-kursat@users.noreply.github.com>
2022-12-02 20:14:11 +0300

    Update SplitNameAndSurname.py

--- SplitNameAndSurname.py
+++ SplitNameAndSurname.py
@@ -2,10 +2,10 @@
 # Kürşat Emre Özkara
 # 02.12.2022
 
-in_student_name = str(input("Öğrenci adı ve soyadı Örnek(Mahmut Tuncer): ")).split(" ")
-print(in_student_name)
+in_student_name = str(input("Öğrenci adı ve soyadı giriniz. Örnek=Mahmut Tuncer: ")).split(" ")
+# print(in_student_name)
 in_student_name = [i.strip() for i in in_student_name if i != ""]
-print(in_student_name)
+# print(in_student_name)
 
 student_name = in_student_name[:-1]
 student_name = " ".join(student_name)
@@ -13,5 +13,5 @@ student_surname = in_student_name[-1]
 
 del in_student_name
 
-print(student_name)
-print(student_surname)
+print(f"İsim: {student_name}")
+print(f"Soyisim: {student_surname}")
